1204f5f451e9e3e24c171a3827cb738ff813a54a
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2019-11-18 10:59:48 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 username=$(whoami)
-sudo cgexec -g memory,cpu:browsers sudo -u $username nodemon index.js 5002 8002 xxxcookie $username token2
+nodemon index.js 5002 8002 xxxcookie $username token2
