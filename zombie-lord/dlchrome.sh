8ee1691066999575af2453198e0cda25df0dc206
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2019-11-21 06:38:01 +0000

    "Update"

--- zombie-lord/dlchrome.sh
+++ zombie-lord/dlchrome.sh
@@ -1,5 +1,5 @@
-rm google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb || :
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt --fix-broken install
 sudo dpkg -i google-chrome-stable_current_amd64.deb 
-rm google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb || :
