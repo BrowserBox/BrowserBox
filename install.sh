06322f43d2da0cf5a4a72ffd90e224fc33b40aa2
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2019-11-21 06:39:26 +0000

    "OK"

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 sudo apt update && sudo apt -y upgrade
 sudo apt install -y curl git wget
 git clone https://github.com/dosycorp/browsergap.ce.git
-sudo apt install -y nodejs npm
-sudo npm i -g npm
+cd browsergap.ce
 ./setup_machine.sh
