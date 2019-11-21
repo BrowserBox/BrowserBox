c1f8601c3be4f6407f387d3502e91126f881a8fa
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2019-11-21 06:38:01 +0000

    "Update"

--- setup_machine.sh
+++ setup_machine.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+sudo apt install -y nodejs npm
+sudo npm i -g npm
 ./install_bundle_deps.sh
 ./global_install_bundle_deps.sh
 cd zombie-lord
+sudo ./dlchrome.sh
 sudo ./deb.deps
 sudo ./font.deps
-sudo ./dlchrome.sh
 cd ..
 npm i
