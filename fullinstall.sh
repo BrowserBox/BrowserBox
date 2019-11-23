9dc0bcee7af61e2d408b84a833ccfdf20df887d1
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2019-11-23 05:12:12 +0000

    "Dockerized"

---
+++ fullinstall.sh
@@ -0,0 +1,15 @@
+sudo apt update && sudo apt -y upgrade
+sudo apt install curl git wget
+
+# Docker 
+
+
+sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+
+sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+
+sudo apt update
+
+
