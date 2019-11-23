18718ad5f487664b7a5b1a247c401e500e3fe963
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2019-11-23 18:23:22 +0800

    Update fullinstall.sh

--- fullinstall.sh
+++ fullinstall.sh
@@ -1,17 +1,22 @@
+
 #!/bin/bash
 
 sudo apt update && sudo apt -y upgrade
 sudo apt install curl git wget
 
-# Docker 
+# Docker
 
 
 sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 
-sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 
 sudo apt update
 
+git clone https://github.com/dosycorp/browsergap.ce.git
+cd browsergap.cd
+./build_docker.sh
+./run_docker.sh
 
