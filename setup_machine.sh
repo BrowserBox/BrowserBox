eefbbc3b128ab637c10af004e04bfbc87618af65
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2019-11-23 23:38:47 +0000

    Good

--- setup_machine.sh
+++ setup_machine.sh
@@ -6,6 +6,7 @@ sudo apt install -y libvips libjpeg-dev
 ./install_bundle_deps.sh
 ./global_install_bundle_deps.sh
 cd zombie-lord
+sudo ./audio.deps
 sudo ./deb.deps
 sudo ./font.deps
 sudo ./dlchrome.sh
