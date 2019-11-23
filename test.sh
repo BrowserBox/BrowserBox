2f49385ee46c8acd23fa6bae7864bb6fca05282e
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2019-11-23 23:32:11 +0000

    Audi oworks

--- test.sh
+++ test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+trap 'sudo kill $BGPID; exit' SIGINT 
+
 username=$(whoami)
+cd parec-server
+node index.js &
+BGPID=$!
+cd ..
 node-dev index.js 5002 8002 xxxcookie $username token2
