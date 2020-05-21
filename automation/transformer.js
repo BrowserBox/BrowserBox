e357ca441da7e4dc4c34bd899db169ec2d95ab28
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-05-21 08:34:44 +0000

    "Sketch automation improvement ideas."

---
+++ automation/transformer.js
@@ -0,0 +1,11 @@
+// A stream transformer
+// to transform an interaction stream
+// into an automation stream
+// by interleaving abstractTabIds
+// and by interleaving selectors
+// into the interqaction stream by applying them to each event that needs them
+
+
+export function transformer(interactionStream) {
+
+}
