f48a3ffb3ec39377de1eef8f3cf607abd4dc39bc
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-07-05 05:08:41 +0000

    "sketching integration of eslint and tsc --checkJS. Re #59"

---
+++ clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -rf node_modules
+cd zombie-lord 
+rm -rf node_modules
+cd ../public/voodoo
+rm -rf node_modules
+cd ../../public
+rm -rf *bundle*
+cd ../zombie-lord/custom-launcher
+rm -rf node_modules
+
+
