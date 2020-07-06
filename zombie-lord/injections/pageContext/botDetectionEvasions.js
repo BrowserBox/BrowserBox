f4b57f598188d2c2d965b5708d37f636f7318a6b
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-07-06 06:12:49 +0000

    "Addressing mouse targeting. Re #57"

--- zombie-lord/injections/pageContext/botDetectionEvasions.js
+++ zombie-lord/injections/pageContext/botDetectionEvasions.js
@@ -9,6 +9,73 @@
 
 {
   /* eslint-disable no-inner-declarations */
+  // temp location pointer targeting diagnois code
+
+	// state and config
+		const Events = `
+			click mousemove mouseover mouseout mousedown pointerdown pointerup pointermove 
+		`.split(/\s+/g).filter(s => s.length);
+		const Styles = {
+			position: 'absolute',
+			pointerEvents: 'none',
+			transform: 'translate(-50%, -50%)',
+			zIndex:999999999,
+			display: 'block',
+			width: '1rem',
+			height: '1rem',
+			background: 'transparent',
+			border: 'medium solid aqua',
+			left: '0px',
+			top: '0px'
+		}
+		const positionMarkers = [];
+
+	install();
+
+	function install() {
+		for( const eventName of Events ) {
+			document.addEventListener(eventName, showPosition);
+		}
+	}
+
+	// display a marker
+		function showPosition({clientX,clientY}) {
+			const div = document.createElement('div');
+			const timestamp = Date.now();
+			Object.assign(div.style, Styles);
+			Object.assign(div.style, {
+				left:clientX+'px', 
+				top:clientY+'px'
+			});
+			document.body.insertAdjacentElement('afterbegin', div);
+			positionMarkers.push({
+				timestamp, 
+				div, 
+				remove(ms) {
+					setTimeout(() => div.remove(), ms);
+				}
+			});
+		}
+
+	// remove markers older than 2 seconds 
+		setInterval(
+			() => {
+				const Now = Date.now();
+				let spliceCount = 0;
+				for( const {timestamp, div, remove} of positionMarkers) {
+					const age = Now - timestamp;
+					if ( age > 500 ) {
+						remove(Math.max(0, 1000 - age));
+						spliceCount++;
+					}
+				}
+				positionMarkers.splice(0, spliceCount);
+			}, 1000);
+
+  /* eslint-enable no-inner-declarations */
+}
+{
+  /* eslint-disable no-inner-declarations */
   // temp location for file input related code in page context
 
   const isFileInput = el => el.localName == 'input' && el.type == 'file';
@@ -16,14 +83,21 @@
   self.zombieDosyLastClicked = {};
 
   self.addEventListener('click', click => {
-    const {target} = click;
-    const {clientX,clientY} = click;
-    const stack = expandShadowRoots(Array.from(document.elementsFromPoint(clientX,clientY)), clientX, clientY);
-    self.zombieDosyLastClicked = {target, stack};
-    const fileInputFromStack = stack.find(isFileInput);
-    self.zombieDosyLastClicked.stack = stack;
-    self.zombieDosyLastClicked.fileInput = (isFileInput(target) && target) || fileInputFromStack;
-    console.log(JSON.stringify({zombieDosyLastClicked:self.zombieDosyLastClicked}));
+      const {target} = click;
+      const {clientX,clientY} = click;
+
+      self.zombieDosyLastClicked = {target};
+
+      const stack = expandShadowRoots(Array.from(document.elementsFromPoint(clientX,clientY)), clientX, clientY);
+
+      if ( isFileInput(target) && target ) {
+        self.zombieDosyLastClicked.fileInput = target;
+      } else {
+        const fileInputFromStack = stack.find(isFileInput);
+        self.zombieDosyLastClicked.fileInput = fileInputFromStack;
+      }
+      
+      //console.log(JSON.stringify({zombieDosyLastClicked:self.zombieDosyLastClicked}));
   }, {capture:true});
 
   function expandShadowRoots(els, x,y ) {
