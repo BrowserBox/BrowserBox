f218b47ce3d165722b961bfc5a60e3ca1c3f7d3e
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-07-15 06:25:52 +0000

    "New release script"

---
+++ new_release.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+description=$1
+gbranch nexe-build
+latest_tag=$(git describe --abbrev=0)
+echo $latest_tag > LAST_RELEASE.txt
+npm run build
+chmod +x BrowserGap.js
+gpush patch "New release"
+latest_tag=$(git describe --abbrev=0)
+grel release -u dosyago -r BrowserGap --tag $latest_tag --name "New release" --description '"'"$description"'"'
+grel upload -u dosyago -r BrowserGap --tag $latest_tag --name "BrowserGap.exe" --file BrowserGap.exe
+grel upload -u dosyago -r BrowserGap --tag $latest_tag --name "BrowserGap.macos" --file BrowserGap.macos
+grel upload -u dosyago -r BrowserGap --tag $latest_tag --name "BrowserGap.linux" --file BrowserGap.nix
+grel upload -u dosyago -r BrowserGap --tag $latest_tag --name "BrowserGap.linx32" --file BrowserGap.nix32
+grel upload -u dosyago -r BrowserGap --tag $latest_tag --name "BrowserGap.win32.exe" --file BrowserGap.win32.exe
+
+
+
