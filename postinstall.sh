5ed95ddd11fb0f8f6397a07cfd460f9d0f023728
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-07-18 10:24:52 +0000

    "OK"

--- postinstall.sh
+++ postinstall.sh
@@ -4,15 +4,15 @@
 # "Cannot find preset babel/env relative to directory public"
 #./install_bundle_deps.sh
 
-npm rebuild --quiet
+npm rebuild &> /dev/null 
 cd zombie-lord 
-npm i --quiet && npm rebuild --quiet
+npm i &> /dev/null  && npm rebuild &> /dev/null 
 cd ../public/voodoo
-npm i --quiet && npm rebuild --quiet
+npm i &> /dev/null  && npm rebuild &> /dev/null 
 # cd ../../endbacker
-# npm i --quiet && npm rebuild --quiet
+# npm i &> /dev/null  && npm rebuild &> /dev/null 
 cd ../../zombie-lord/custom-launcher
-npm i --quiet && npm rebuild --quiet
+npm i &> /dev/null  && npm rebuild &> /dev/null 
 cd ../../
 
 
