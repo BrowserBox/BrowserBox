09ce7d7fe532c5ec56e4fc2b48ad7fc047d3c277
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-07-21 01:38:24 +0000

    New release

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: "./server.js",
+  output: {
+    path: path.resolve(__dirname),
+    filename: "BrowserGap.js"
+  },
+  target: "node",
+  node: {
+    __dirname: false
+  },
+  plugins: [
+    new webpack.BannerPlugin({ banner: "#!/usr/bin/env node", raw: true }),
+  ]
+};
