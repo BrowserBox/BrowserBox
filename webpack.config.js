91a7e84444ff37a6935e64a4830264b4fc0e208d
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-07-21 12:47:01 +0000

    "OK"

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname),
+    filename: "BrowserGap.js"
+  },
+  target: "node",
+  node: {
+    __dirname: false
+  },
+  plugins: [
+    new webpack.BannerPlugin({ banner: "#!/usr/bin/env node", raw: true }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.node$/,
+        loader: 'node-loader',
+      },
+    ],
+  },
+};
