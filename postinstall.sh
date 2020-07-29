a1a6fb223fece8f2a128a156927b6f4826b0d470
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-07-29 04:35:41 +0000

    "OK"

--- postinstall.sh
+++ postinstall.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# killing bundling for now until work out babel issue
-# "Cannot find preset babel/env relative to directory public"
-#./install_bundle_deps.sh
-
 npm rebuild &> /dev/null 
 cd zombie-lord 
 npm i &> /dev/null  && npm rebuild &> /dev/null 
