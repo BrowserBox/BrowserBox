c792c902603e1dabb9df7c5de5934f55677bdfd7
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-07-29 04:35:04 +0000

    "Improved install script"

--- postinstall.sh
+++ postinstall.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-# killing bundling for now until work out babel issue
-# "Cannot find preset babel/env relative to directory public"
-#./install_bundle_deps.sh
+echo 
+echo
+read -p "Want to run setup_machine script? (you only need to do this the first time you install BG, or when you update new version) y/n " -n 1 -r
+echo
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Not running full setup...Just doing npm install..."
+else 
+  echo "Running full setup..."
+  ./setup_machine.sh
+fi
+
 
 npm rebuild &> /dev/null 
 cd zombie-lord 
@@ -12,8 +22,10 @@ npm i &> /dev/null  && npm rebuild &> /dev/null
 # cd ../../endbacker
 # npm i &> /dev/null  && npm rebuild &> /dev/null 
 cd ../../zombie-lord/custom-launcher
+
 npm i &> /dev/null  && npm rebuild &> /dev/null 
 cd ../../
 
-./make_bundle.sh
+./make_bundle.sh &> /dev/null
+
 
