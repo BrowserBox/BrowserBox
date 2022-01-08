3205306ad08b10d9ca859af4220fe8b02e816318
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2022-01-08 07:02:47 +0000

    "Tidying"

--- automation/corrector.js
+++
@@ -1,8 +0,0 @@
-// A stream transformer
-// to transform an automation stream
-// into a correct version
-// by correcting selectors through generalization
-
-export function corrector(/*interactionStream*/) {
-
-}
