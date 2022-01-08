e2a97e34066d029ab355dff0ed8bb8b0394f729f
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2022-01-08 16:40:51 +0000

    "Testing binary builds"

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 module.exports = {
   entry: "./src/server.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '..', 'dist'),
     filename: "Viewfinder.js"
   },
   target: "node",
