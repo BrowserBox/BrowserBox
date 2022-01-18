730289c15dfdad9c8b879cd1641bc8ca172f1bec
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2022-01-18 15:05:31 +0000

    better install

--- src/zombie-lord/dlchrome.sh
+++ src/zombie-lord/dlchrome.sh
@@ -1,4 +1,7 @@
-which google-chrome-stable || sudo apt install google-chrome-stable
-sudo apt-get install -f
-sudo apt --fix-broken -y install
+if (which google-chrome-stable || sudo apt install google-chrome-stable); then
+  sudo apt-get install -f
+  sudo apt --fix-broken -y install
+else 
+  ./../../scripts/dpkg_dlchrome.sh
+fi
 
