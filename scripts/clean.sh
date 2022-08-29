741176a0d79c8c75496ce1950c132d6c870a8c4f
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2022-08-29 22:28:22 +0800

    Fixed

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,15 +4,23 @@ rm ./build/*
 rm ./dist/*
 rm -rf typetests
 rm -rf node_modules
+rm package-lock.json
 cd ./src/zombie-lord 
 rm -rf node_modules
+rm package-lock.json
 cd ../../
 cd ./src/public/voodoo
 rm -rf node_modules
+rm package-lock.json
 cd ../../../
 cd ./src/public
 rm -f *bundle*.js
 cd ../../
 cd ./src/zombie-lord/custom-launcher
 rm -rf node_modules
+rm package-lock.json
+cd ../../
+cd ./parec-server
+rm -rf node_modules
+rm package-lock.json
 
