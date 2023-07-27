d444de79eb1e58a9416d2bb09b546e5ed7675a47
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-07-27 08:00:01 +0000

    Seems to have flags that work now

--- deploy-scripts/build_docker.sh
+++ deploy-scripts/build_docker.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+if [[ ! -d node_modules ]]; then
+  echo "no" | npm i
+fi
 npm run parcel
 docker buildx build --platform linux/amd64 -t bbpro-v4 . > artefacts/build.log 2>&1 &
 tail -f artefacts/build.log
