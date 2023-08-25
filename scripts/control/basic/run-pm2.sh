f6ee79dcc228776809ef188f65a8e89494533801
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-08-25 04:30:28 +0000

    OK

--- scripts/control/basic/run-pm2.sh
+++ scripts/control/basic/run-pm2.sh
@@ -5,8 +5,8 @@ echo "Starting viewfinder service cluster as $username"
 
 get_install_dir() {
   echo "Finding bbpro installation..."
-  install_path1=$(find /usr/local/share -name .bbpro_install_dir -print -quit 2>/dev/null)
-  install_path2=$(find $HOME -name .bbpro_install_dir -print -quit 2>/dev/null)
+  install_path1=$(find $HOME -name .bbpro_install_dir -print -quit 2>/dev/null)
+  install_path2=$(find /usr/local/share -name .bbpro_install_dir -print -quit 2>/dev/null)
   install_dir=$(dirname $install_path1)
   if [ -z "$install_dir" ]; then 
     install_dir=$(dirname $install_path2)
