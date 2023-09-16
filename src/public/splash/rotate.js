fe551bda214e89e6ab8be84b3bd49829906c45e1
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-09-16 15:15:59 +0800

    Great. Counter clockwise rotation

--- src/public/splash/rotate.js
+++ src/public/splash/rotate.js
@@ -3,7 +3,7 @@ let rotation = 0;
 function animateGradient() {
   const ring = document.getElementById('ring');
   rotation = (rotation + 1) % 360;
-  ring.style.rotate = `${rotation}deg`;
+  ring.style.rotate = `${-rotation}deg`;
   requestAnimationFrame(animateGradient);
 }
 // Initialize the animation
