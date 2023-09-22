0342c9a74a6ac71edaee46cfc8f9a8d44a946c41
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-09-22 23:20:44 +0800

    OK

--- src/services/pool/deploy/scripts/setup.sh
+++ src/services/pool/deploy/scripts/setup.sh
@@ -17,8 +17,6 @@ if ! command -v pm2; then
   $SUDO bash -c "source ~/.nvm/nvm.sh; npm i -g pm2@latest"
 fi
 
-echo -n "Set up quota user to have 5G 5G and 2m 2m block and inode limit."
-read -p " Enter to continue "
 $SUDO cp ./scripts/commands/* /usr/local/bin/
 
 # Edit the $SUDOers file to allow members of the "renice" group to run the "renice" command
@@ -28,5 +26,14 @@ then
   echo "%renice ALL=NOPASSWD: /usr/bin/renice, /usr/bin/loginctl, /usr/bin/id" | $SUDO tee -a /etc/$SUDOers >&2
 fi
 
-$SUDO ./scripts/commands/newuser.sh quotauser
+
+if [ "$GITHUB_ACTIONS" == "true" ]; then
+  echo "we are in github action, and the doc viewer will not be accessible, so we don't set it up. This is normal."
+  exit 0
+fi
+
+#I don't think we need these anymore
+#echo -n "Set up quota user to have 5G 5G and 2m 2m block and inode limit."
+#read -p " Enter to continue "
+#$SUDO ./scripts/commands/newuser.sh quotauser
 
