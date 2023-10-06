7b96588f9ab28c3034674e96c0e362798c5625ac
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-10-07 00:01:34 +0800

    Fix chai

--- src/services/pool/chai/scripts/flexbashrunner.sh
+++ src/services/pool/chai/scripts/flexbashrunner.sh
@@ -29,11 +29,6 @@ is_rosetta() {
 # Check platform
 platform=$(get_platform)
 
-echo "1: $1" >&2
-echo "2: $2" >&2
-echo "3: $3" >&2 
-echo "4: $4" >&2
-
 # Original script and its arguments
 original_script="$1"
 shift  # Remove the first argument to get only the remaining arguments
@@ -47,10 +42,10 @@ else
   if is_rosetta; then
     echo "Rosetta on Mac so executing with right arch" >&2
     # If running under Rosetta, switch to arm64
-    arch -arm64 "${bash_location}" -c "source $HOME/.bashrc; source $HOME/.profile; \"${original_script}\" $@"
+    arch -arm64 "${bash_location}" -c "source $HOME/.bashrc; source $HOME/.profile; \"${original_script}\" \"$1\" \"$2\" \"$3\""
   else
     # Otherwise, run normally
-    "${original_script}" "$@"
+    "${original_script}" "$1" "$2" "$3"
   fi
 fi
 
