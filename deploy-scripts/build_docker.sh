a60ff48695d45b4768c92763cdb90b05327f9a97
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-10-17 02:12:37 +0800

    Test 2mr

--- deploy-scripts/build_docker.sh
+++ deploy-scripts/build_docker.sh
@@ -4,5 +4,5 @@ if [[ ! -d node_modules ]]; then
   echo "no" | npm i
 fi
 npm run parcel
-DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64 -t bbpro-v4 . > artefacts/build.log 2>&1 &
+DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64 -t bbpro-v5 . > artefacts/build.log 2>&1 &
 tail -f artefacts/build.log
