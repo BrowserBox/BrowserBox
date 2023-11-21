7334e172cca618d2a48205de377000bae3d6d873
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-11-21 14:58:05 +0800

    SW does not work on Tor browser unfortunately due to restrictions. But massively speeds up load in other browsers. Yay

--- src/public/register_sw.js
+++ src/public/register_sw.js
@@ -1,12 +1,11 @@
 import {CONFIG} from './voodoo/src/common.js';
 
 if (CONFIG.useServiceWorkerToCache && 'serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js') // Adjust the path as necessary
-    .then(registration => {
-      console.log('Service Worker registered with scope:', registration.scope);
-    })
-    .catch(error => {
-      console.error('Service Worker registration failed:', error);
-    });
+  navigator.serviceWorker.register('/sw.js').then(registration => {
+    console.log('Service Worker registered with scope:', registration.scope);
+  })
+  .catch(error => {
+    console.error('Service Worker registration failed:', error);
+  });
 }
 
