977d6a71d5be7ee7ba03b58d65c3e0050c9748b2
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-12-10 15:32:22 +0800

    Non interactive

--- deploy-scripts/non-interactive.sh
+++ deploy-scripts/non-interactive.sh
@@ -4,16 +4,19 @@
 echo "Creating dpkg configuration..."
 sudo mkdir -p /etc/dpkg/dpkg.cfg.d/
 cat <<EOF | sudo tee /etc/dpkg/dpkg.cfg.d/force-conf >/dev/null
-confdef
-confold
+force-confdef
+force-confnew
 EOF
 
 # Create apt configuration
 echo "Creating apt configuration..."
 sudo mkdir -p /etc/apt/apt.conf.d/
-cat <<EOF | sudo tee /etc/apt/apt.conf.d/99force-yes >/dev/null
+cat <<EOF | sudo tee /etc/apt/apt.conf.d/99non-interactive >/dev/null
 APT::Get::Assume-Yes "true";
-APT::Get::force-yes "true";
+APT::Get::allow-unauthenticated "true";
+APT::Get::allow-downgrades "true";
+APT::Get::allow-remove-essential "true";
+APT::Get::allow-change-held-packages "true";
 EOF
 
 echo "Configurations applied successfully."
