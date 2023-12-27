378d05e8b283c3edd023824c614571f77adb4ffe
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-12-27 13:42:36 +0800

    Setup nice

--- deploy-scripts/_setup_bbpro.ps1
+++ deploy-scripts/_setup_bbpro.ps1
@@ -1,81 +1,42 @@
+# Get the current script path
+$scriptPath = $MyInvocation.ScriptName
 
-# _setup_bbpro.ps1 PowerShell Script
-
-# Install Guard for OpenSSL
-if (-Not (Get-Command openssl -ErrorAction SilentlyContinue)) {
-  Write-Host "OpenSSL is not installed. Installing now..."
-  winget install -e --id ShiningLight.OpenSSL
-} else {
-  Write-Host "OpenSSL is already installed."
+# Function to check if running as administrator
+function Is-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 }
-# Check if the port is available and also the two ports on either side
-Function Test-PortRange {
-  param([int]$port)
-  $portsToCheck = @($port - 2, $port - 1, $port, $port + 1, $port + 2)
-  foreach ($p in $portsToCheck) {
-    $tcpconnection = Test-NetConnection -ComputerName localhost -Port $p -ErrorAction SilentlyContinue
-    if ($tcpconnection.TcpTestSucceeded -eq $true) {
-      Write-Host "Error: Port $p is already in use."
-      return $false
+
+# Determine the PowerShell executable to use
+$psExecutable = if ($PSVersionTable.PSVersion.Major -ge 6) { "pwsh" } else { "powershell" }
+
+# Restart the script as administrator if not already
+if (-not (Is-Admin)) {
+    try {
+        $arguments = "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`" $args"
+        Start-Process $psExecutable -Verb RunAs -ArgumentList $arguments
+        exit
     }
-  }
-  return $true
+    catch {
+        Write-Error "Failed to start as Administrator: $_"
+        exit
+    }
 }
 
-# Check if the port and adjacent ports are free
-if (-Not (Test-PortRange -port $Port)) {
-  Write-Host "Error: One of the ports adjacent to $Port is already in use."
-  exit 1
-}
-# Parse command-line arguments for port, token, cookie, and doc API key
-param (
-  [int]$Port,
-  [string]$Token,
-  [string]$Cookie,
-  [string]$DocApiKey
-)
-# Check if port is provided
-if ($null -eq $Port) {
-  Write-Host "Error: You must provide a port number."
-  exit 1
-}
-# Generate random values if not provided
-if ($null -eq $Token) {
-  $Token = -join ((65..90) + (97..122) | Get-Random -Count 32 | ForEach-Object { [char]$_ })
-}
-if ($null -eq $Cookie) {
-  $Cookie = -join ((65..90) + (97..122) | Get-Random -Count 32 | ForEach-Object { [char]$_ })
-}
-if ($null -eq $DocApiKey) {
-  $DocApiKey = -join ((65..90) + (97..122) | Get-Random -Count 32 | ForEach-Object { [char]$_ })
-}
-# Validate the provided or generated values
-# Note: This is a placeholder; you'll need to replace it with actual logic
-# Create a configuration directory and a test.env file with the parsed options
-$ConfigDir = "$env:USERPROFILE\.config\dosyago\bbpro"
-if (-Not (Test-Path -Path $ConfigDir)) {
-  New-Item -Path $ConfigDir -ItemType Directory -Force
-}
-# Generate test.env file
-$testEnvContent = @"
-export APP_PORT=$Port
-export LOGIN_TOKEN=$Token
-export COOKIE_VALUE=$Cookie
-export DOC_API_KEY=$DocApiKey
-"@
-$testEnvPath = Join-Path $ConfigDir "test.env"
-Set-Content -Path $testEnvPath -Value $testEnvContent
-# Parse the hostname from the SSL certificate
-$sslCertsPath = "$env:USERPROFILE\sslcerts"
-$certFile = Join-Path $sslCertsPath "fullchain.pem"
-if (Test-Path -Path $certFile) {
-  # Use OpenSSL to extract the SANs
-  $sans = & openssl x509 -in $certFile -noout -text | Select-String "DNS:" -Context 0,1
-  $hostname = ($sans -split ",")[0] -replace "DNS:", ""
-  $loginLink = "https://$hostname:$Port/login?token=$Token"
-  Write-Host "The login link for this instance will be: $loginLink"
-  $loginLinkPath = Join-Path $ConfigDir "login.link"
-  Set-Content -Path $loginLinkPath -Value $loginLink
-} else {
-  Write-Host "Could not find certificate file at $certFile"
-}
+# If running as administrator, execute the bash script with the script's arguments
+$bashScript = "./deploy-scripts/_setup_bbpro.sh"
+$arguments = $args -join ' '
+$processInfo = New-Object System.Diagnostics.ProcessStartInfo
+$processInfo.FileName = "bash"
+$processInfo.RedirectStandardOutput = $true
+$processInfo.UseShellExecute = $false
+$processInfo.Arguments = "$bashScript $arguments"
+$process = New-Object System.Diagnostics.Process
+$process.StartInfo = $processInfo
+$process.Start() | Out-Null
+$output = $process.StandardOutput.ReadToEnd()
+$process.WaitForExit()
+
+# Print the output
+Write-Host $output
+
