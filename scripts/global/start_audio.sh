5bdec19f57e30dbd36af129fffb6d8a2481e36de
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-12-30 13:36:21 +0800

    fix some win stuff

--- scripts/global/start_audio.sh
+++ scripts/global/start_audio.sh
@@ -5,11 +5,21 @@ node="$(command -v node.exe)"
 PLATFORM_IS=$("$node" -p "process.platform")
 
 # Check for PulseAudio only if not on Windows
-if [[ $PLATFORM_IS != win* ]] && ! command -v pulseaudio &>/dev/null; then
-  echo "Sorry, we are not on Windows and PulseAudio is not installed. Audio will not work." >&2
-  echo "Therefore, we will not try the audio service." >&2
-  echo "Exiting..." >&2
-  exit 0
+if [[ $PLATFORM_IS == win* ]]; then
+  if ! command -v pulseaudio.exe &>/dev/null; then
+    echo "Sorry, PulseAudio for Windows by Patrick Gaskin is not installed. Audio will not work." >&2
+    echo "(ensure you add the PulseAudio bin\ directory to the system path)" >&2
+    echo "Therefore, we will not try the audio service." >&2
+    echo "Exiting..." >&2
+    exit 1
+  fi
+else 
+  if ! command -v pulseaudio &>/dev/null; then
+    echo "Sorry, PulseAudio is not installed. Audio will not work." >&2
+    echo "Therefore, we will not try the audio service." >&2
+    echo "Exiting..." >&2
+    exit 1
+  fi
 fi
 
 if [[ -z "${BB_POOL}" ]] || [[ $PLATFORM_IS == win* ]]; then
--- scripts/stop-test.sh
+++ scripts/stop-test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 command -v pm2 &>/dev/null || npm i -g pm2@latest
-node=$(command -v node.exe)
+node="$(command -v node.exe || command -v node)"
 pm2 delete start_audio 
 pm2 delete basic-bb-main-service 
 pm2 delete devtools-server 
 pm2 delete run-docspark
 
-PLATFORM_IS=$("$node" -p process.platform)
+PLATFORM_IS="$("$node" -p process.platform)"
 if [[ $PLATFORM_IS == win* ]]; then
  pwsh=$(command -v pwsh || command -v powershell)
  "$pwsh" -Command "taskkill /F /IM node.exe"
