bfdd0b7572282a46684c509643403ed03d03d468
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2024-01-04 16:57:48 +0800

    notes

--- windows-scripts/BrowserBox-Windows-Install.ps1
+++ windows-scripts/BrowserBox-Windows-Install.ps1
@@ -1,4 +1,7 @@
 # add arguments for -acceptTermsEmail <email@address> -hostname <bb host> that will also be passed through ($args)
+# extend GUI to account for these which will not show if args are provided implying consent
+# add wait for hostname to resolve function and console reminder to add A record
+# if hostname resolves to a link local address ensure that it points at our machine. How? don't know. But can be done.
 param (
   [string]$UserPassword
 )
