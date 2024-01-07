43632fcaa7fa369d09156b4e366b569ceb57a372
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2024-01-07 20:46:44 +0800

    Good

--- windows-scripts/BrowserBox-Tools/Install-BrowserBox.ps1
+++ windows-scripts/BrowserBox-Tools/Install-BrowserBox.ps1
@@ -3,6 +3,6 @@
 # add wait for hostname to resolve function and console reminder to add A record
 # if hostname resolves to a link local address ensure that it points at our machine. How? don't know. But can be done.
 function Install-BrowserBox {
-  .\Install-BrowserBox-Task.ps1
+  . $PSScriptPath\Install-BrowserBox-Task.ps1
 }
 
