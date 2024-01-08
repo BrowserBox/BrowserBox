9e508506611e5d04d14dccfc3413614462ef9b0e
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2024-01-08 13:48:25 +0800

    Add instructs

--- windows-scripts/BrowserBox-Tools/Initialize-BrowserBox.ps1
+++ windows-scripts/BrowserBox-Tools/Initialize-BrowserBox.ps1
@@ -17,6 +17,8 @@ function Initialize-BrowserBox {
     # If Token is not provided, exclude it from the command
     $loginLink = & ./deploy-scripts/_setup_bbpro.ps1 --port $Port
   }
+  Write-Host "Please note: we will open the required ports in Windows Firewall, but if you're cloud or hosting provider uses an external firewall ensure you have opened firewall ports $($Port-2)-$($Port+2) in your machine's control panel"
+  Write-Host "Next steps: Start-BrowserBox"
   Write-Output $loginLink
   Set-Location $originalDirectory
 }
