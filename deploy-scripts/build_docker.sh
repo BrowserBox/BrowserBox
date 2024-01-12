c111b1db0140dd007472d2a374ad727667a7e044
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2024-01-12 17:58:49 +0800

    Auto tag

--- deploy-scripts/build_docker.sh
+++ deploy-scripts/build_docker.sh
@@ -5,6 +5,7 @@ if [[ ! -d node_modules ]]; then
 fi
 npm run parcel
 DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64 -t bbpro . > artefacts/build.log 2>&1 &
+docker tag bbpro ghcr.io/browserbox/browserbox:latest
 tail -f artefacts/build.log
 
 
