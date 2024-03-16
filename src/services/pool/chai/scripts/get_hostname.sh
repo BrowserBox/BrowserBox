c1a9805e468b0f63e24dd89dd01d1b7fcbe72132
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2024-03-16 13:28:00 +0000

    ok

--- src/services/pool/chai/scripts/get_hostname.sh
+++ src/services/pool/chai/scripts/get_hostname.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
 # Define the path to the SSL certificates
-ssl_dir="$HOME/sslcerts"
+ssl_dir="${SSLCERTS_DIR:-"${HOME}/sslcerts"}"
 
 output=""
+
+get_external_ip() {
+  local ip
+
+  # List of services to try
+  local services=(
+    "https://icanhazip.com"
+    "https://ifconfig.me"
+    "https://api.ipify.org"
+  )
+
+  # Try each service in turn
+  for service in "${services[@]}"; do
+    ip=$(curl -4s --connect-timeout 5 "$service")
+    if [[ -n "$ip" ]]; then
+      echo "$ip"
+      return
+    fi
+  done
+
+  echo "Failed to obtain external IP address" >&2
+  return 1
+}
 
 # Check if the SSL certificates exist
 if [[ -f "$ssl_dir/privkey.pem" && -f "$ssl_dir/fullchain.pem" ]]; then
@@ -13,7 +36,7 @@ if [[ -f "$ssl_dir/privkey.pem" && -f "$ssl_dir/fullchain.pem" ]]; then
   output="$hostname"
 else
   # Get the IP address (you can also use other methods to get the IP)
-  ip_address=$(hostname -I | awk '{print $1}')
+  ip_address=$(get_external_ip)
   echo "IP Address: $ip_address" >&2
   output="$ip_address"
 fi
