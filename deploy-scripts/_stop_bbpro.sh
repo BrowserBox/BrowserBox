9de4afe458704d3265b126f69e0dd894648a1b02
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2024-03-21 14:09:02 +0000

    ok

--- deploy-scripts/_stop_bbpro.sh
+++ deploy-scripts/_stop_bbpro.sh
@@ -3,7 +3,10 @@
 source ~/.nvm/nvm.sh
 
 command -v pm2 &>/dev/null || npm i -g pm2@latest
-pm2 delete all
+pm2 delete basic-bb-main-service
+pm2 delete run-docspark
+pm2 delete devtools-server
+pm2 delete start_audio
 killall node chrome
 
 exit 0 
