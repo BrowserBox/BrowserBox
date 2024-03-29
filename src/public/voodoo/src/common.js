78df2a6a7e5ea98a59c4e7ae96bf55a639074989
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2024-03-29 05:56:51 +0000

    Setup for dev

--- src/public/voodoo/src/common.js
+++ src/public/voodoo/src/common.js
@@ -1,7 +1,7 @@
 export const SERVICE_COUNT = 4; // pptr(menu), chat, audio, devtools
 export const FRAME_CONTROL = false;
 
-export const VERSION = '8.7.2';
+export const VERSION = '8.7.3';
 export const SafariPlatform = /^((?!chrome|android).)*safari/i;
 const MobilePlatform = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
 const FirefoxPlatform = /firefox/i;
@@ -46,7 +46,7 @@ export const DEBUG = Object.freeze({
                                                 // mobile will not peer webrtc unless we get perms so no point trying 
                                                 // if user media fails
   utilizeTempHackFixForIMENoKey: true,
-  mode: 'prod',
+  mode: 'dev',
   debugKeyEvents: false,
   debugCommandOrder: false,
   // note on: increaseResolutionOfSmallerCanvas
@@ -85,7 +85,8 @@ export const DEBUG = Object.freeze({
   HFUNCTION: false,
   debugCopyPaste: false,
   trackLoading: true,
-  debugAudioAck: false,
+  debugAudio: true,
+  debugAudioAck: true,
   debugFastest: false,
   get useStraightAudioStream() {
     return globalThis?.location?.host?.endsWith?.('.onion') || false;
@@ -104,7 +105,6 @@ export const DEBUG = Object.freeze({
   showCollect: false,
   debugFocus: false,
   debugAuth: false,
-  debugAudio: false,
   debugModal: false,
   get debugMeta() {
     return  (this.debugFavicon > 1) || 0;
