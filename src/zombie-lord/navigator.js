59c386dd9583c4d2a2fde7c7f60e4fb625fc39e2
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2024-04-03 11:54:19 +0800

    Update user agents

--- src/zombie-lord/navigator.js
+++ src/zombie-lord/navigator.js
@@ -2,12 +2,12 @@
 // firefox is the best UA to use for compatibility across most sites
 // I found that if I use Chrome or Safari UAs, websites will behave differently
 
-export const deskUA_Mac_Chrome = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-export const deskUA_Mac_FF = "Mozilla/5.0 (Macintosh; Intel Mac OS X 12.6; rv:105.0) Gecko/20100101 Firefox/105.0";
+export const deskUA_Mac_Chrome = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+export const deskUA_Mac_FF = "Mozilla/5.0 (Macintosh; Intel Mac OS X 14.4; rv:124.0) Gecko/20100101 Firefox/124.0";
 
-export const mobUA_iOSFF = "Mozilla/5.0 (iPhone; CPU iPhone OS 12_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/120.0 Mobile/15E148 Safari/605.1.15";
-export const mobUA_AndroidFF = "Mozilla/5.0 (Android 13; Mobile; rv:68.0) Gecko/68.0 Firefox/120.0";
-export const mobUA_iOSSafari = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_0_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Mobile/15E148 Safari/604.1";
+export const mobUA_iOSFF = "Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/124.0 Mobile/15E148 Safari/605.1.15";
+export const mobUA_AndroidFF = "Mozilla/5.0 (Android 14; Mobile; rv:124.0) Gecko/124.0 Firefox/124.0";
+export const mobUA_iOSSafari = "Mozilla/5.0 (iPhone; CPU iPhone OS 17_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.3.1 Mobile/15E148 Safari/604.1";
 
 export const LANG = "en-US";
 
