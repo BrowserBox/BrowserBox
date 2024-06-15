dc40e4c30e8db44603bf3fa495057e952900096c
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2024-06-15 16:33:27 +0800

    Adding top up button

--- src/common.js
+++ src/common.js
@@ -154,7 +154,7 @@ export const DEBUG = Object.freeze({
   chooseFastest: !process.env.TORBB && true,
   logCastOutOfOrderFrames: false,
   noSecurityHeaders: false,
-  mode: 'prod', // prod or dev (whether to bundle frontend code or not)
+  mode: 'dev', // prod or dev (whether to bundle frontend code or not)
   showOrigin: false,
   useDocCustomDownloadPlugin: true,
   useFlashEmu: process.env.USE_FLASH == 'true' ? true : false,
