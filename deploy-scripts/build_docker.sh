9b63a74beef399d2f1e9052d6dea4281eb466ca8
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2024-08-30 11:35:53 +0800

    Small increases

--- deploy-scripts/build_docker.sh
+++ deploy-scripts/build_docker.sh
@@ -5,8 +5,9 @@
 if [[ ! -d node_modules ]]; then
   yes | npm i
 fi
+tag="$(git tag | tail -n 1)"
 npm run bundle
 DOCKER_BUILDKIT=1 
 docker buildx create --use
-docker buildx build --push --platform linux/amd64,linux/arm64 -t ghcr.io/browserbox/browserbox:latest -t dosyago/browserbox:latest . 
+docker buildx build --push --platform linux/amd64,linux/arm64 -t ghcr.io/browserbox/browserbox:latest -t "ghcr.io/browserbox/browserbox:${tag}" -t dosyago/browserbox:latest -t "dosyago/browserbox:${tag}" . 
 
