9a5ed0f3a2ed2d1f4729c61f6b67f85117a5883c
DOSAYGO Development Team <development.team@dosyago.com>
2024-10-02 20:00:02 +0800

    ONSW

--- src/public/voodoo/src/common.js
+++ src/public/voodoo/src/common.js
@@ -222,7 +222,7 @@ export const CONFIG = Object.freeze({
   useTopLevelControlKeyListeners: true,
   useTopLevelSendKeyListeners: true,
   get useServiceWorkerToCache() {
-    return false;
+    return true;
   },
   downloadMeterVanishTimeout: DEBUG.debugDownload ? 500000 : 5000,
   ACK_BLAST_LENGTH: 1000,
