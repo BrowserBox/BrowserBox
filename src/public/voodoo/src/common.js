34df9ac23f38dc0e5cbf0208db2968680d2b8b83
DOSAYGO Engineering <development.team@dosyago.com>
2024-10-14 11:17:30 +0800

    Fix modal issues and tab switching lockup regression - and use SW

--- src/public/voodoo/src/common.js
+++ src/public/voodoo/src/common.js
@@ -1,4 +1,4 @@
-const USW = false; // service worker
+const USW = true; // service worker
 export const VERSION = '10.3.5';
 export const SERVICE_COUNT = 4; // browser, documents, audio, devtools
 export const FRAME_CONTROL = false;
