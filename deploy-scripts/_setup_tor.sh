2f9d0cde084bf976b7fdcdacb705fbde60ca5b52
DOSAYGO Engineering <development.team@dosyago.com>
2025-03-17 18:32:27 +0800

    fix

--- deploy-scripts/_setup_tor.sh
+++ deploy-scripts/_setup_tor.sh
@@ -155,7 +155,7 @@ restart_tor_service() {
   if $RESTART_TOR; then
     echo "Restarting Tor service..." >&2
     if [[ "$OS_TYPE" == "macos" ]]; then
-      $SUDO su - $SUD_USER -c "bash -cl 'brew services restart tor'"
+      $SUDO su - $SUDO_USER -c "bash -cl 'brew services restart tor'"
     else
       systemctl restart "$TOR_SERVICE"
       # Check if we're in Docker or systemd isn't working
