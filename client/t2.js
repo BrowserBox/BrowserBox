74021597ff5ffedbca7e467081c5d97f06126d45
DOSAYGO Engineering <development.team@dosyago.com>
2025-03-25 23:55:38 +0800

    perfect

--- client/t2.js
+++ client/t2.js
@@ -25,6 +25,8 @@ const tabData = [
 ];
 let tabOffset = 0; // For scrolling
 let focusedTabIndex = 0; // Focused tab
+let selectedTabIndex = -1; // Selected tab (-1 means none selected)
+let cursorPosition = 0; // Cursor position in address bar
 const TAB_MIN_WIDTH = 25;
 const TAB_HEIGHT = 1;
 const OMNIBOX_HEIGHT = 3;
@@ -40,8 +42,10 @@ function drawTabs() {
     term.moveTo(x, 1);
     if (i === focusedTabIndex) {
       term.bgWhite()[tabData[i].color]().bold().underline(tabText); // Focused tab
+    } else if (i === selectedTabIndex) {
+      term.bgBlue()[tabData[i].color]().underline(tabText); // Selected tab
     } else {
-      term.bgBlue()[tabData[i].color](tabText); // Unfocused tab
+      term.bgBlue()[tabData[i].color](tabText); // Unfocused, unselected tab
     }
     x += tabText.length;
   }
@@ -81,7 +85,11 @@ function drawOmnibox(focusedElement = null, addressContent = '') {
   // Address bar
   term.moveTo(BACK_WIDTH + FORWARD_WIDTH + 2, TAB_HEIGHT + 2);
   if (focusedElement === 'address') {
-    term.bgBrightWhite().black().bold(addressContent.padEnd(ADDRESS_WIDTH, ' '));
+    const beforeCursor = addressContent.slice(0, cursorPosition);
+    const cursorChar = addressContent[cursorPosition] || ' ';
+    const afterCursor = addressContent.slice(cursorPosition + 1);
+    term.bgBrightWhite().black().bold(beforeCursor + cursorChar + afterCursor.padEnd(ADDRESS_WIDTH - beforeCursor.length - 1, ' '));
+    term.moveTo(BACK_WIDTH + FORWARD_WIDTH + 2 + cursorPosition, TAB_HEIGHT + 2).bgBrightWhite().black().bold(cursorChar); // Highlight cursor
   } else {
     term.bgWhite().black(addressContent.slice(0, ADDRESS_WIDTH).padEnd(ADDRESS_WIDTH, ' '));
   }
@@ -112,103 +120,203 @@ render(focusedElement, addressContent);
 term.grabInput(true);
 
 term.on('key', async (key) => {
-  switch (key) {
-    case 'CTRL_C':
-    case 'q':
-      term.clear();
-      term.processExit(0);
-      break;
-
-    case 't': // Add new tab
-      tabData.push({ name: `New ${tabData.length + 1}`, color: 'white' });
-      render(focusedElement, addressContent);
-      break;
-
-    case 'LEFT':
-      if (focusedElement === 'tabs') {
-        if (tabOffset > 0) {
-          tabOffset--;
-          focusedTabIndex = tabOffset; // Sync focus with visible tab
-          render(focusedElement, addressContent);
-        }
-      } else if (focusedElement === 'forward') {
-        focusedElement = 'back';
-        render(focusedElement, addressContent);
-      } else if (focusedElement === 'address') {
-        focusedElement = 'forward';
-        render(focusedElement, addressContent);
-      } else if (focusedElement === 'go') {
-        focusedElement = 'address';
-        render(focusedElement, addressContent);
-      }
-      break;
-
-    case 'RIGHT':
-      if (focusedElement === 'tabs') {
-        if (tabOffset < tabData.length - MAX_VISIBLE_TABS) {
-          tabOffset++;
-          focusedTabIndex = tabOffset; // Sync focus with visible tab
-          render(focusedElement, addressContent);
-        }
-      } else if (focusedElement === 'back') {
-        focusedElement = 'forward';
-        render(focusedElement, addressContent);
-      } else if (focusedElement === 'forward') {
-        focusedElement = 'address';
-        render(focusedElement, addressContent);
-      } else if (focusedElement === 'address') {
+  if (focusedElement === 'address') {
+    // Address bar mode: limited key handling
+    switch (key) {
+      case 'TAB':
         focusedElement = 'go';
         render(focusedElement, addressContent);
-      }
-      break;
+        break;
 
-    case 'UP': // Move focus to tabs
-      if (focusedElement !== 'tabs') {
-        focusedElement = 'tabs';
+      case 'SHIFT_TAB':
+        focusedElement = 'forward';
         render(focusedElement, addressContent);
-      }
-      break;
+        break;
 
-    case 'DOWN': // Move focus to omnibox
-      if (focusedElement === 'tabs') {
-        focusedElement = 'back';
-        render(focusedElement, addressContent);
-      }
-      break;
-
-    case 'TAB': // Cycle focus within omnibox when not on tabs
-      if (focusedElement !== 'tabs') {
-        if (focusedElement === 'back') focusedElement = 'forward';
-        else if (focusedElement === 'forward') focusedElement = 'address';
-        else if (focusedElement === 'address') focusedElement = 'go';
-        else if (focusedElement === 'go') focusedElement = 'back';
-        render(focusedElement, addressContent);
-      }
-      break;
-
-    case 'ENTER':
-      if (focusedElement === 'tabs') {
-        term.windowTitle(`Selected: ${tabData[focusedTabIndex].name}`);
-      } else if (focusedElement === 'back') {
-        term.windowTitle('Back pressed');
-      } else if (focusedElement === 'forward') {
-        term.windowTitle('Forward pressed');
-      } else if (focusedElement === 'go' || focusedElement === 'address') {
+      case 'ENTER':
         term.windowTitle(`Navigating to: ${addressContent}`);
-      }
-      render(focusedElement, addressContent);
-      break;
+        render(focusedElement, addressContent);
+        break;
 
-    default:
-      if (focusedElement === 'address') {
-        if (key === 'BACKSPACE') {
-          addressContent = addressContent.slice(0, -1);
-        } else if (key.length === 1) {
-          addressContent += key;
+      case 'LEFT':
+        if (cursorPosition > 0) {
+          cursorPosition--;
+          render(focusedElement, addressContent);
+        }
+        break;
+
+      case 'RIGHT':
+        if (cursorPosition < addressContent.length) {
+          cursorPosition++;
+          render(focusedElement, addressContent);
+        }
+        break;
+
+      case 'BACKSPACE':
+        if (cursorPosition > 0) {
+          addressContent = addressContent.slice(0, cursorPosition - 1) + addressContent.slice(cursorPosition);
+          cursorPosition--;
+          render(focusedElement, addressContent);
+        }
+        break;
+
+      default:
+        if (key.length === 1) {
+          addressContent = addressContent.slice(0, cursorPosition) + key + addressContent.slice(cursorPosition);
+          cursorPosition++;
+          render(focusedElement, addressContent);
+        }
+        break;
+    }
+  } else {
+    // Normal mode: full key handling
+    switch (key) {
+      case 'CTRL_C':
+      case 'q':
+        term.clear();
+        term.processExit(0);
+        break;
+
+      case 't': // Add new tab
+        tabData.push({ name: `New ${tabData.length + 1}`, color: 'white' });
+        render(focusedElement, addressContent);
+        break;
+
+      case 'LEFT':
+      case 'h': // Move focus left across tabs
+        if (focusedElement === 'tabs') {
+          if (focusedTabIndex > 0) {
+            focusedTabIndex--;
+            if (focusedTabIndex < tabOffset) {
+              tabOffset--;
+            }
+            render(focusedElement, addressContent);
+          }
+        } else if (focusedElement === 'forward') {
+          focusedElement = 'back';
+          render(focusedElement, addressContent);
+        } else if (focusedElement === 'address') {
+          focusedElement = 'forward';
+          render(focusedElement, addressContent);
+        } else if (focusedElement === 'go') {
+          focusedElement = 'address';
+          render(focusedElement, addressContent);
+        }
+        break;
+
+      case 'RIGHT':
+      case 'l': // Move focus right across tabs
+        if (focusedElement === 'tabs') {
+          if (focusedTabIndex < tabData.length - 1) {
+            focusedTabIndex++;
+            if (focusedTabIndex >= tabOffset + MAX_VISIBLE_TABS) {
+              tabOffset++;
+            }
+            render(focusedElement, addressContent);
+          }
+        } else if (focusedElement === 'back') {
+          focusedElement = 'forward';
+          render(focusedElement, addressContent);
+        } else if (focusedElement === 'forward') {
+          focusedElement = 'address';
+          render(focusedElement, addressContent);
+        } else if (focusedElement === 'address') {
+          focusedElement = 'go';
+          render(focusedElement, addressContent);
+        }
+        break;
+
+      case 'UP':
+      case 'k': // Move focus to tabs
+        if (focusedElement !== 'tabs') {
+          focusedElement = 'tabs';
+          render(focusedElement, addressContent);
+        }
+        break;
+
+      case 'DOWN':
+      case 'j': // Move focus to omnibox
+        if (focusedElement === 'tabs') {
+          focusedElement = 'back';
+          render(focusedElement, addressContent);
+        }
+        break;
+
+      case 'TAB': // Cycle through everything forward
+        if (focusedElement === 'tabs') {
+          if (focusedTabIndex < tabData.length - 1) {
+            focusedTabIndex++;
+            if (focusedTabIndex >= tabOffset + MAX_VISIBLE_TABS) {
+              tabOffset++;
+            }
+          } else {
+            focusedElement = 'back';
+          }
+        } else if (focusedElement === 'back') {
+          focusedElement = 'forward';
+        } else if (focusedElement === 'forward') {
+          focusedElement = 'address';
+          cursorPosition = addressContent.length; // Start at end of text
+        } else if (focusedElement === 'go') {
+          focusedElement = 'tabs';
+          focusedTabIndex = 0;
+          tabOffset = 0;
         }
         render(focusedElement, addressContent);
-      }
-      break;
+        break;
+
+      case 'SHIFT_TAB': // Cycle through everything backward
+        if (focusedElement === 'tabs') {
+          if (focusedTabIndex > 0) {
+            focusedTabIndex--;
+            if (focusedTabIndex < tabOffset) {
+              tabOffset--;
+            }
+          } else {
+            focusedElement = 'go';
+          }
+        } else if (focusedElement === 'back') {
+          focusedElement = 'tabs';
+          focusedTabIndex = tabData.length - 1;
+          tabOffset = Math.max(0, tabData.length - MAX_VISIBLE_TABS);
+        } else if (focusedElement === 'forward') {
+          focusedElement = 'back';
+        } else if (focusedElement === 'go') {
+          focusedElement = 'address';
+          cursorPosition = addressContent.length; // Start at end of text
+        }
+        render(focusedElement, addressContent);
+        break;
+
+      case 'ENTER':
+        if (focusedElement === 'tabs') {
+          selectedTabIndex = focusedTabIndex; // Select the focused tab
+          term.windowTitle(`Selected: ${tabData[selectedTabIndex].name}`);
+          render(focusedElement, addressContent);
+        } else if (focusedElement === 'back') {
+          term.windowTitle('Back pressed');
+          render(focusedElement, addressContent);
+        } else if (focusedElement === 'forward') {
+          term.windowTitle('Forward pressed');
+          render(focusedElement, addressContent);
+        } else if (focusedElement === 'go') {
+          term.windowTitle(`Navigating to: ${addressContent}`);
+          render(focusedElement, addressContent);
+        }
+        break;
+
+      default:
+        if (key === 'r' && focusedElement === 'tabs') { // Bonus r key
+          if (focusedTabIndex > 0) {
+            focusedTabIndex--;
+            if (focusedTabIndex < tabOffset) {
+              tabOffset--;
+            }
+            render(focusedElement, addressContent);
+          }
+        }
+        break;
+    }
   }
 });
 
