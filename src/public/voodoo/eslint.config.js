4794f38831aaceba0af7534053f209065717e348
DOSAYGO Engineering <development.team@dosyago.com>
2025-04-24 04:09:17 -0400

    ok

--- src/public/voodoo/eslint.config.js
+++ src/public/voodoo/eslint.config.js
@@ -2,8 +2,18 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
+  {
+    ignores: [
+      "**/*bundle*",
+      "**/.bang.html.snapshot/**",
+      "old/",
+      "plugins/demo/",
+      "plugins/appminifier/",
+      "src/handlers/demo.js",
+      "**/simplepeer.min.js"
+    ]
+  },
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
 ]);
