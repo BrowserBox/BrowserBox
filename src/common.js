1f960c26fea7c3b219db6fa71f855aae55003a6b
DOSAYGO Engineering <development.team@dosyago.com>
2025-08-30 11:32:33 +0800

    ok

--- src/common.js
+++ src/common.js
@@ -18,9 +18,8 @@ export const scratchState = {
   slowConnection: false,
 };
 process.env.BB_QUICK_EXIT = process.env.STATUS_MODE == "quick exit" ? "true" : "";
-export const HVAL = await rainstormHash(256, 999, process.env.STATUS_MODE);
-export const KILL_TIME = process.env.BB_QUICK_EXIT ? 18_222 : (HVAL == "02e4219509566f8f44a9842cb12c0c55dbb80f50f0d0a76842231127df92541d" ? 3600_000 : 422_222 );
-console.log({HVAL});
+export const HVAL = await rainstormHash(256, 999, process.env.STATUS_MODE || "");
+export const KILL_TIME = process.env.BB_QUICK_EXIT ? 18_222 : (HVAL == "d3c6102b47c111b661e4de4995ef0cec2bee4177461df22fbac9d02772b00a30" ? 3600_000 : 422_222 );
 export const T2_MINUTES = 2 * 60; // 2 minutes in seconds
 export const StartupTabs = new Set(); // track tabs that arrive at setup
 export const OurWorld = new Map();
