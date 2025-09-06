e2ce073f9598368e1b205ff8413afa53d705a9c7
DOSAYGO Engineering <development.team@dosyago.com>
2025-09-06 13:55:35 +0800

    ok

--- src/public/voodoo/expose_externals.sh
+++ src/public/voodoo/expose_externals.sh
@@ -11,10 +11,10 @@ protecc_win_sysadmins() {
 # Call the function right away
 protecc_win_sysadmins
 
-cp -r node_modules/bang.html/* .bang.html.snapshot/
+#cp -r node_modules/bang.html/* .bang.html.snapshot/
 
 # Find and delete all LICENSE files in .bang.html.snapshot to avoid any excuse for confusion with BB license as we save this useful view library in BB repo for posterity (DOSAYGO owns the library)
-find .bang.html.snapshot -type f -name "LICENSE" -delete
+#find .bang.html.snapshot -type f -name "LICENSE" -delete
 
 cp node_modules/simple-peer/simplepeer.min.js src/ 
 cp node_modules/lucide-static/icons/*.svg assets/icons/
